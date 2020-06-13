38bb5374cb9fee7c14440f0e54ddf76e28ba0db1
Victor <angvictor91@gmail.com>
2020-06-13 14:39:24 +0700

    remove hotelid unique in rent

--- src/models/RentModel.js
+++ src/models/RentModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const rentSchema = mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    hotel: {type: mongoose.Schema.Types.ObjectId, ref: 'Hotel', unique: true},
+    hotel: {type: mongoose.Schema.Types.ObjectId, ref: 'Hotel'},
     room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room'},
     rentDate: { type: String, default: ""}
 }, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
